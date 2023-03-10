15365c930e2d0283983a0ede52a1d8153afd1290
acesyde <acesyde@gmail.com>
2023-03-10 11:51:31 +0100

    hass: dev

---
+++ rollup.config.mjs
@@ -0,0 +1,53 @@
+import babel from "@rollup/plugin-babel";
+import commonjs from "@rollup/plugin-commonjs";
+import json from "@rollup/plugin-json";
+import nodeResolve from "@rollup/plugin-node-resolve";
+import terser from "@rollup/plugin-terser";
+import typescript from "@rollup/plugin-typescript";
+import serve from "rollup-plugin-serve";
+
+const dev = process.env.ROLLUP_WATCH;
+
+const serveOptions = {
+  contentBase: ["./dist"],
+  host: "0.0.0.0",
+  port: 4000,
+  allowCrossOrigin: true,
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+  },
+};
+
+const plugins = [
+  typescript({
+    declaration: false,
+  }),
+  nodeResolve(),
+  json(),
+  commonjs(),
+  babel({
+    babelHelpers: "bundled",
+  }),
+  ...(dev ? [serve(serveOptions)] : [terser()]),
+];
+
+export default [
+  {
+    input: "src/rounded.ts",
+    output: {
+      dir: "dist",
+      format: "es",
+      inlineDynamicImports: true,
+    },
+    plugins,
+    moduleContext: (id) => {
+      const thisAsWindowForModules = [
+        "node_modules/@formatjs/intl-utils/lib/src/diff.js",
+        "node_modules/@formatjs/intl-utils/lib/src/resolve-locale.js",
+      ];
+      if (thisAsWindowForModules.some((id_) => id.trimRight().endsWith(id_))) {
+        return "window";
+      }
+    },
+  },
+];
