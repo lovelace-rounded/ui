db85f9915064477822c4a13fe3c3be5d265860d1
acesyde <acesyde@gmail.com>
2023-03-11 07:55:38 +0000

    fix: dev

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -7,13 +7,13 @@ set -e
 ###################################
 
 echo "============= 📦 Submodules 📦 ============="
-#rm -R homeassistant-frontend
-#rm -R rounded-theme
-#git submodule update --init --recursive --remote
+rm -R homeassistant-frontend
+rm -R rounded-theme
+git submodule update --init --recursive --remote
 
 echo "============= 📦 Python requirements 📦 ============="
-#pip install -r requirements_dev.txt
-#pre-commit install --install-hooks
+pip install -r requirements_dev.txt
+pre-commit install --install-hooks
 
 echo "============= 📦 Pnpm install 📦 ============="
 cd "homeassistant-frontend/" && pnpm install
