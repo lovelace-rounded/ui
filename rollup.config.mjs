cad3c366162ddb48d178092420176c3504bcca6e
acesyde <acesyde@gmail.com>
2023-03-11 08:27:09 +0000

    fix: build

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -6,6 +6,16 @@ import nodeResolve from "@rollup/plugin-node-resolve";
 import terser from "@rollup/plugin-terser";
 import typescript from "@rollup/plugin-typescript";
 import serve from "rollup-plugin-serve";
+import ignore from "./rollup-plugins/ignore-plugin.js";
+
+const IGNORED_FILES = [
+  "@material/mwc-ripple/mwc-ripple.js",
+  // "@material/mwc-list/mwc-list.js",
+  // "@material/mwc-list/mwc-list-item.js",
+  // "@material/mwc-menu/mwc-menu.js",
+  // "@material/mwc-menu/mwc-menu-surface.js",
+  // "@material/mwc-icon/mwc-icon.js",
+];
 
 const dev = process.env.ROLLUP_WATCH;
 
@@ -20,6 +30,9 @@ const serveOptions = {
 };
 
 const plugins = [
+  ignore({
+    files: IGNORED_FILES.map((file) => require.resolve(file)),
+  }),
   alias({
     entries: [
       { find: /^lit\/decorators$/, replacement: "lit/decorators.js" },
@@ -37,27 +50,15 @@ const plugins = [
         replacement: "lit/directives/if-defined.js",
       },
     ],
-    // entries: {
-    //   "lit/decorators$": "lit/decorators.js",
-    //   "lit/directive$": "lit/directive.js",
-    //   "lit/directives/until$": "lit/directives/until.js",
-    //   "lit/directives/class-map$": "lit/directives/class-map.js",
-    //   "lit/directives/style-map$": "lit/directives/style-map.js",
-    //   "lit/directives/if-defined$": "lit/directives/if-defined.js",
-    //   "lit/directives/guard$": "lit/directives/guard.js",
-    //   "lit/directives/cache$": "lit/directives/cache.js",
-    //   "lit/directives/repeat$": "lit/directives/repeat.js",
-    //   "lit/polyfill-support$": "lit/polyfill-support.js",
-    //   "@lit-labs/virtualizer/layouts/grid":
-    //     "@lit-labs/virtualizer/layouts/grid.js",
-    // },
   }),
   typescript({
     declaration: false,
   }),
   nodeResolve(),
   json(),
-  commonjs(),
+  commonjs({
+    sourceMap: false,
+  }),
   babel({
     babelHelpers: "bundled",
   }),
